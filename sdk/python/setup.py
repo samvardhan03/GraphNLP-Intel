ec04837e69ed4e802570f074b1bf02adcb44bb2b
samvardhan03 <shekhawatsamvardhan@gmail.com>
2026-04-18 20:53:40 +0530

    docs: attach README.md to PyPI deployments for packages

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup, find_packages
 setup(
     name="graphnlp-client",
-    version="0.1.1",
+    version="0.1.3",
     packages=find_packages(),
+    long_description=open("../../README.md").read(),
+    long_description_content_type="text/markdown",
     install_requires=["httpx>=0.27"],
 )
